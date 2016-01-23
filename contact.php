8a7d9fff5fb35822afcb043608e06abfaed9ba8f
Yaron <y.lambers@outlook.com>
2016-01-23 04:51:03 +0100

    style

---
+++ contact.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div class="menubar">
+    <img src="img/omega.png" width="200px" height="200px" />
+</div>
+<div class="container">
+    <a href="index.php">Home</a>
+    <a href="contact.php">Contact</a>
+
+    <a href="/login/index.php"> Login</a>
+
+
+    <div class="content">
+        <h1>Contact</h1>
+        <h3>Adress</h3>
+        <p>
+            1725 West Street
+            Greensburg, PA 15601
+        </p>
+
+        <h3>Contact information</h3>
+        <p>
+            Email: omega@hotmail.com
+        </p>
+
+        <p>
+            Tell: 06-209233344
+            Fax: 06-209233344
+        </p>
+
+
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,9 +1,34 @@
-<a href="login/index.php"> login</a>
-<?php
-/**
- * Created by PhpStorm.
- * User: Yaron
- * Date: 20-1-2016
- * Time: 22:08
- */
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div class="menubar">
+    <img src="img/omega.png" width="200px" height="200px" />
+</div>
+<div class="container">
+    <a href="index.php">Home</a>
+    <a href="contact.php">Contact</a>
 
+    <a href="/login/index.php"> Login</a>
+
+
+    <div class="content">
+        <h1>Omega</h1>
+
+        <p>
+            The Omega Workshops Ltd. was a design enterprise founded by members of the Bloomsbury Group and established in July 1913.[1] It was located at 33 Fitzroy Square in London, and was founded with the intention of providing graphic expression to the essence of the Bloomsbury ethos.[2] The Workshops were also closely associated with the Hogarth Press and the artist and critic Roger Fry, who was the principal figure behind the project, believed that artists could design, produce and sell their own works, and that writers could also be their own printers and publishers.[3] The Directors of the firm were Fry, Duncan Grant and Vanessa Bell.[1]
+        </p>
+
+        <h2>For beginners</h2>
+        <p>
+            Fry aimed to remove what he considered to be the false divisions between the decorative and fine arts, and to give his artist friends an additional income opportunity in designing furniture, textiles and other household accessories. Fry was keen to encourage a Post-Impressionist influence in designs produced for Omega. However, Cubist and Fauvist influences are also apparent, particularly in many of the textile designs.
+
+            To ensure items were bought only for the quality of the work, and not the reputation of the artist, Fry insisted works be shown anonymously, marked only with the letter omega. The products were in general expensive, and aimed at an exclusive market.
+        </p>
+
+        <img src="img/Nina_Hamnett.jpg" height="400px" width="250px" />
+    </div>
+</div>
